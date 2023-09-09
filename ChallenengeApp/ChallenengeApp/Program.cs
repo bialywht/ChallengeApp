52e9f7cddfb30413730a019ef1e72d986ec44c85
bialywht <bialywht@gmail.com>
2023-09-10 00:10:36 +0200

    zadanie, dzień trzeci

--- ChallenengeApp/ChallenengeApp/Program.cs
+++ ChallenengeApp/ChallenengeApp/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var imie = "Maciej";
+var plec = "M";
+var wiek = 43;
+
+if (plec != "M")
+{
+    if (wiek < 33)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+
+}
+else
+{
+    if (wiek < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Maciej, lat 43");
+    }
+}
