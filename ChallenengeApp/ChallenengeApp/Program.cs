d0ab53af8c97d9f9582ba7f0600746c28bbbd435
bialywht <bialywht@gmail.com>
2023-09-15 20:52:47 +0200

    poprawione ify

--- ChallenengeApp/ChallenengeApp/Program.cs
+++ ChallenengeApp/ChallenengeApp/Program.cs
@@ -1,20 +1,24 @@
-﻿var name = "Teresa";
+﻿var name = "Ewa";
 var gender = "F";
-var age = 60;
+var age = 30;
 
 if (gender == "F")
 {
-    if (age == 33 && name == "Ewa")
-    {
-        Console.WriteLine("Ewa, lat 33");
-    }
-    else if (age < 30)
+
+    if (age < 30)
     {
         Console.WriteLine("Kobieta poniżej 30 lat");
     }
-    else if (age >= 30)
+    else if (age >= 30 && age < 33 || age > 33)
     {
-        Console.WriteLine("Kobieta powyżej 30 lat, ale nie Ewa");
+        Console.WriteLine("Kobieta powyżej 30 lat, niebędąca 33-letnią Ewą");
+    }
+    else
+    {
+        if (name == "Ewa")
+        {
+            Console.WriteLine("Ewa, lat 33");
+        }
     }
 
 }
